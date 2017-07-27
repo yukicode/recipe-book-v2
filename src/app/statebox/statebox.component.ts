15058da12c35ea2568ebc12427a6c9634481cec8
yukicode <yolandaapply@gmail.com>
2017-07-26 22:45:37 -0700

    remove the testing part of statebox

--- src/app/statebox/statebox.component.ts
+++ src/app/statebox/statebox.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-statebox',
   templateUrl: './statebox.component.html',
   styleUrls: ['./statebox.component.css'],
 })
-export class StateboxComponent implements OnInit {
+export class StateboxComponent {
   @Input() config: any;
 
   defaultConfig = {
@@ -18,12 +18,4 @@ export class StateboxComponent implements OnInit {
   constructor() {
     if(!this.config) { this.config = this.defaultConfig; }
   }
-
-  ngOnInit() {
-  }
-
-  changeState() {
-    this.config.show = !this.config.show;
-  }
-
 }
